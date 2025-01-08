3f37b1d25c456daef50d152f06183f2d691e32e7
Alex Bright <alexander.bright@johnlewis.co.uk>
2025-01-08 17:30:50 +0000

    Updating `is_prime` method to make it faster

--- methods.py
+++ methods.py
@@ -12,13 +12,10 @@ def get_proper_divisors(number_to_check):
 
 # from math import floor, sqrt
 def is_prime(number_to_check):
-    divisors = set()
-    for divisor in range(floor(sqrt(number_to_check)), 0, -1):
+    for divisor in range(floor(sqrt(number_to_check)), 2, -1):
         if number_to_check % divisor == 0:
-            divisors.add(divisor)
-            divisors.add(int(number_to_check / divisor))
-    divisors.remove(number_to_check)
-    return len(divisors) == 1
+            return False
+    return True
 
 # from math import floor
 def is_palindromic_number(number):
